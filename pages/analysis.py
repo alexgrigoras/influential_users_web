380cc09d11afe321fded19994bef5e10c3f7635b
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-05-30 15:32:35 +0300

    Added description, alerts for success and failure, slider for nr of videos

--- pages/analysis.py
+++ pages/analysis.py
@@ -11,6 +11,18 @@ from application.youtube_api import YoutubeAPI
 from server import app
 from utilities.auth import layout_auth
 
+success_alert = dbc.Alert(
+    'Finished searching',
+    color='success',
+    dismissable=True
+)
+
+failure_alert = dbc.Alert(
+    'Parameters are invalid',
+    color='danger',
+    dismissable=True
+)
+
 login_alert = dbc.Alert(
     'User not logged in. Taking you to login.',
     color='danger'
@@ -25,34 +37,54 @@ n = True
 def layout():
     # if current_user.is_authenticated:
     return dbc.Row(
-        [
-            dbc.Col(
-                children=[
-                    location,
-                    html.Div(id='home-login-trigger', style=dict(display='none')),
-
-                    html.H3('Search'),
-                    html.Br(),
-
-                    html.H5("Keyword: "),
-                    dcc.Input(id='keyword', value='', type='text'),
-                    html.Br(), html.Br(),
-                    html.H5("Nr videos: "),
-                    dcc.Input(id='nr_videos', value='', type='number'),
-                    html.Br(), html.Br(),
-                    dbc.Button('Search', color='primary', id='submit-button'),
-                    html.Br(), html.Br(),
-                    dcc.Loading(
-                        id="loading-1",
-                        type="graph",
-                        children=html.Div(id="loading-output-1")
-                    )
-                ],
-                width=3
-            ),
-            dbc.Col(html.Div(id='output-div'))
-        ]
+        dbc.Col(
+            children=[
+                location,
+                html.Div(id='home-login-trigger', style=dict(display='none')),
+                html.H3('Analyze YouTube Social Media'),
+                html.Br(),
+                html.H5('What the application does: '),
+                html.Ul(
+                    [
+                        html.Li("Searches the YouTube platform for videos regarding the search keyword"),
+                        html.Li("Downloads the necessary data from videos (title, description, comments and others)"),
+                        html.Li("Creates a graph by representing the connections between users that commented on "
+                                "selected videos"),
+                        html.Li("Computes PageRank to determine the most important users"),
+                        html.Li("Displays the created graph and the results from PageRank")
+                    ]
+                ),
+                html.H5('To use the application, enter the keyword and the number of videos and click SEARCH'),
+                html.Div(id='output-alert'),
+                html.H5("Keyword: "),
+                dcc.Input(id='keyword', value='', type='text', placeholder="keyword"),
+                html.Br(), html.Br(),
+                html.H5("Number of videos: "), html.Div(id="range-val"),
+                dcc.Input(id='nr_videos', value='1', type='range', placeholder="Valid from 1 to 10", min=1, max=10,
+                          step=1),
+                html.Br(), html.Br(),
+                dbc.Button('Search', color='primary', id='submit-button'),
+                html.Br(), html.Br(),
+                dcc.Loading(
+                    id="loading-1",
+                    type="graph",
+                    children=html.Div(id="loading-output-1")
+                ),
+                html.Br(),
+                html.Hr(),
+                html.Br(),
+                dbc.Col(html.Div(id='output-div'))
+            ],
+            align="center"
+        ),
+        justify="center"
     )
+
+
+@app.callback(Output("range-val", "children"),
+              [Input('nr_videos', 'value')])
+def input_triggers_spinner(value):
+    return value
 
 
 @app.callback(Output("loading-output-1", "children"), [Input('submit-button', 'n_clicks')])
@@ -62,18 +94,25 @@ def input_triggers_spinner(value):
     return ''
 
 
-@app.callback(Output('output-div', 'children'),
+@app.callback([Output('output-div', 'children'),
+               Output('output-alert', 'children')],
               [Input('submit-button', 'n_clicks')],
               [State('keyword', 'value'),
                State('nr_videos', 'value')])
 def update_output(clicks, keyword, nr_videos):
     if clicks is not None:
-        print("Searching for data with keyword " + keyword)
-
         global n
+
+        if not keyword or not nr_videos:
+            print("invalid")
+            n = False
+            return '', failure_alert
+
+        print("Searching for data with keyword " + keyword)
 
         n = True
 
+        # create crawler and network object
         crawler = YoutubeAPI()
         network = NetworkAnalysis()
 
@@ -89,8 +128,6 @@ def update_output(clicks, keyword, nr_videos):
         ranks = network.compute_page_rank()
         # network.compute_betweenness_centrality()
 
-        # network.display_tree()
-        # network.display_graphviz()
         fig = network.display_plotly()
 
         columns = ['Value', 'Name']
@@ -108,10 +145,13 @@ def update_output(clicks, keyword, nr_videos):
                 dcc.Graph(
                     id='network-graph',
                     figure=fig),
+                html.Br(),
+                html.Hr(),
+                html.Br(),
                 create_data_table_network(values, columns)
             ],
             id='dash-container'
-        )
+        ), success_alert
 
 
 def create_data_table_network(values, columns):

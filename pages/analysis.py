74b8c594b423337578f696568bae329612dc1627
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-07-08 13:21:12 +0300

    Added more options for creating the graphs, added message logging

--- pages/analysis.py
+++ pages/analysis.py
@@ -5,6 +5,7 @@ import dash_table
 from dash.dependencies import Output, Input, State
 from flask_login import current_user
 
+from application.message_logger import MessageLogger
 from application.network_analysis import NetworkAnalysis
 from application.youtube_api import YoutubeAPI
 from server import app
@@ -28,6 +29,8 @@ login_alert = dbc.Alert(
 )
 
 location = dcc.Location(id='analysis-url', refresh=True, pathname='/analysis')
+ml = MessageLogger('analysis')
+logger = ml.get_logger()
 
 
 @layout_auth('require-authentication')
@@ -51,14 +54,29 @@ def layout():
                         html.Li("Displays the created graph and the results from PageRank")
                     ]
                 ),
-                html.H5('To use the application, enter the keyword and the number of videos and click SEARCH'),
+                html.H6('[DISCLAIMER] The application loses its state if the page is reloaded or closed!'),
+                html.Br(),
+                html.H5('To use the application, enter the options below and click SEARCH'),
                 html.Div(id='output-alert'),
                 html.H5("Keyword: "),
                 dcc.Input(id='keyword', value='', type='text', placeholder="keyword"),
                 html.Br(), html.Br(),
-                html.H5("Number of videos: "), html.Div(id="range-val"),
+                html.H5("Number of videos: "), html.Div(id="range-val-videos"),
                 dcc.Input(id='nr_videos', value='1', type='range', placeholder="Valid from 1 to 10", min=1, max=10,
                           step=1),
+                html.H5("Number of users: "), html.Div(id="range-val-users"),
+                dcc.Input(id='nr_users', value='30', type='range', placeholder="Valid from 1 to 100", min=1, max=100,
+                          step=1),
+                html.H5("Graph type: "),
+                dcc.RadioItems(
+                    id="graph_type",
+                    options=[
+                        {'label': "3D Spring", 'value': '3'},
+                        {'label': "2D Graph", 'value': '2'}
+                    ],
+                    value='3',
+                    labelStyle={'display': 'inline-block', "margin-right": "15px"}
+                ),
                 html.Br(), html.Br(),
                 dbc.Button('Search', color='primary', id='submit-button'),
                 html.Br(), html.Br(),
@@ -78,8 +96,14 @@ def layout():
     )
 
 
-@app.callback(Output("range-val", "children"),
+@app.callback(Output("range-val-videos", "children"),
               [Input('nr_videos', 'value')])
+def input_triggers_spinner(value):
+    return value
+
+
+@app.callback(Output("range-val-users", "children"),
+              [Input('nr_users', 'value')])
 def input_triggers_spinner(value):
     return value
 
@@ -89,19 +113,21 @@ def input_triggers_spinner(value):
                Output("loading-output-1", "children")],
               [Input('submit-button', 'n_clicks')],
               [State('keyword', 'value'),
-               State('nr_videos', 'value')])
-def update_output(clicks, keyword, nr_videos):
+               State('nr_videos', 'value'),
+               State('nr_users', 'value'),
+               State('graph_type', 'value')])
+def update_output(clicks, keyword, nr_videos, nr_users, graph_type):
     if clicks is not None:
-        if not keyword or not nr_videos:
-            print("invalid data")
+        if not keyword or not nr_videos or not nr_users or not graph_type:
+            logger.error("Invalid user input data")
             return '', failure_alert, ''
 
         if current_user.is_authenticated:
-            print("user authenticated")
+            logger.info("User " + current_user.first + " " + current_user.last + " authenticated")
         else:
-            print("user not authenticated")
+            logger.warning("User not authenticated")
 
-        print("Searching for data with keyword " + keyword)
+        limit = int(nr_users)
 
         # create crawler and network object
         crawler = YoutubeAPI()
@@ -115,32 +141,42 @@ def update_output(clicks, keyword, nr_videos):
         # create users network
         network.set_file_name(file_name)
         network.create_network()
-        labels = network.getLabels()
+        labels = network.get_labels()
         ranks = network.compute_page_rank()
         # network.compute_betweenness_centrality()
 
-        fig = network.display_plotly()
-
         columns = ['Value', 'Name']
         values = []
-        for i in ranks:
-            val = {"Value": check_value(ranks, i), 'Name': check_value(labels, i)}
+        index = 0
+        selected_nodes = []
+        for u_id in sorted(ranks, key=ranks.get, reverse=True):
+            selected_nodes.append(u_id)
+            val = {"Value": check_value(ranks, u_id), 'Name': check_value(labels, u_id)}
             values.append(val)
+            index += 1
+            if index >= limit:
+                break
+
+        fig = network.display_plotly(selected_nodes, graph_type)
 
         if send_finished_process_confirmation(current_user.email, current_user.first, keyword):
-            print("Confirmation sent to " + current_user.email + " " + current_user.first)
+            logger.info("Confirmation sent to " + current_user.email + " " + current_user.first)
         else:
-            print("Confirmation NOT sent to " + current_user.email + " " + current_user.first)
+            logger.warning("Confirmation NOT sent to " + current_user.email + " " + current_user.first)
 
         # Create Layout
         return html.Div(
             children=[
                 html.Div(id='my-div'),
+                html.Br(),
+                html.H5("Users Graph"),
                 dcc.Graph(
                     id='network-graph',
                     figure=fig),
                 html.Br(),
                 html.Hr(),
+                html.Br(),
+                html.H5("Users Table"),
                 html.Br(),
                 create_data_table_network(values, columns)
             ],

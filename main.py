929da42b221eb398eb716bba2a3fe70f72fa3d0c
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-07-08 13:20:10 +0300

    Added logging for server and changed menu bar user name

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# index page
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from flask_login import current_user
 
+from application.message_logger import MessageLogger
 from pages import (
     home,
     profile,
     analysis
 )
-
 # app authentication
 from pages.auth_pages import (
     login,
@@ -19,7 +18,7 @@ from pages.auth_pages import (
     forgot_password,
     change_password
 )
-from server import app, server
+from server import app
 
 header = dbc.Navbar(
     dbc.Container(
@@ -28,10 +27,10 @@ header = dbc.Navbar(
                             href="/home"),
             dbc.Nav(
                 [
-                    dbc.NavItem(dbc.NavLink("Home", href="/home", style={"color":"white"})),
-                    dbc.NavItem(dbc.NavLink("Analysis", href="/analysis", style={"color":"white"})),
-                    dbc.NavItem(dbc.NavLink(id='user-name', href='/profile', style={"color":"white"})),
-                    dbc.NavItem(dbc.NavLink('Login', id='user-action', href='Login', style={"color":"white"}))
+                    dbc.NavItem(dbc.NavLink("Home", href="/home", style={"color": "white"})),
+                    dbc.NavItem(dbc.NavLink("Analysis", href="/analysis", style={"color": "white"})),
+                    dbc.NavItem(dbc.NavLink(id='user-name', href='/profile', style={"color": "white"})),
+                    dbc.NavItem(dbc.NavLink('Login', id='user-action', href='Login', style={"color": "white"}))
                 ]
             )
         ]
@@ -94,7 +93,7 @@ def profile_link(content):
     returns a navbar link to the user profile if the user is authenticated
     """
     if current_user.is_authenticated:
-        return html.Div(current_user.first)
+        return html.Div("[" + current_user.first + "]")
     else:
         return ''
 
@@ -103,7 +102,7 @@ def profile_link(content):
     [Output('user-action', 'children'),
      Output('user-action', 'href')],
     [Input('page-content', 'children')])
-def user_logout(input1):
+def user_logout(user_input):
     """
     returns a navbar link to /logout or /login, respectively, if the user is authenticated or not
     """
@@ -114,4 +113,7 @@ def user_logout(input1):
 
 
 if __name__ == '__main__':
+    ml = MessageLogger('werkzeug')
+    handler = ml.get_handler()
+    app.logger.addHandler(handler)
     app.run_server(host='0.0.0.0', port=5000, debug=False)

b726c426c9efd1f657ba2ee4831c755ea3a67f1f
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-07-09 21:06:11 +0300

    Updated layout to cards

--- pages/analysis.py
+++ pages/analysis.py
@@ -42,36 +42,47 @@ logger = ml.get_logger()
 @layout_auth('require-authentication')
 def layout():
     # if current_user.is_authenticated:
-    return dbc.Row(
-        dbc.Col(
-            children=[
-                location,
+    text_card = dbc.Card(
+        dbc.CardBody(
+            [
+                html.H4("Analyze YouTube Social Media", className="card-title"),#location,
                 html.Div(id='home-login-trigger', style=dict(display='none')),
-                html.H3('Analyze YouTube Social Media'),
-                html.Br(),
                 html.H5('What the application does: '),
                 html.Ul(
                     [
                         html.Li("Searches the YouTube platform for videos regarding the search keyword"),
-                        html.Li("Downloads the necessary data from videos (title, description, comments and others)"),
-                        html.Li("Creates a graph by representing the connections between users that commented on "
-                                "selected videos"),
+                        html.Li(
+                            "Downloads the necessary data from videos (title, description, comments and others)"),
+                        html.Li(
+                            "Creates a graph by representing the connections between users that commented on "
+                            "selected videos"),
                         html.Li("Computes PageRank to determine the most important users"),
                         html.Li("Displays the created graph and the results from PageRank")
                     ]
                 ),
                 html.H6('[DISCLAIMER] The application loses its state if the page is reloaded or closed!'),
                 html.Br(),
-                html.H5('To use the application, enter the options below and click SEARCH'),
+                html.H5('To use the application, enter the options on the right and click SEARCH'),
+            ]
+        ),
+        color="info", outline=True
+    )
+
+    search_card = dbc.Card(
+        dbc.CardBody(
+            [
+                html.H4("Search", className="card-title"),
                 html.Div(id='output-alert'),
                 html.H5("Keyword: "),
                 dcc.Input(id='keyword', value='', type='text', placeholder="keyword"),
                 html.Br(), html.Br(),
                 html.H5("Number of videos: "), html.Div(id="range-val-videos"),
-                dcc.Input(id='nr_videos', value='1', type='range', placeholder="Valid from 1 to 10", min=1, max=10,
+                dcc.Input(id='nr_videos', value='1', type='range', placeholder="Valid from 1 to 10", min=1,
+                          max=10,
                           step=1),
                 html.H5("Number of users: "), html.Div(id="range-val-users"),
-                dcc.Input(id='nr_users', value='30', type='range', placeholder="Valid from 1 to 100", min=1, max=100,
+                dcc.Input(id='nr_users', value='30', type='range', placeholder="Valid from 1 to 100", min=1,
+                          max=100,
                           step=1),
                 html.H5("Graph type: "),
                 dcc.RadioItems(
@@ -83,22 +94,47 @@ def layout():
                     value='3',
                     labelStyle={'display': 'inline-block', "margin-right": "15px"}
                 ),
-                html.Br(), html.Br(),
                 dbc.Button('Search', color='primary', id='submit-button'),
-                html.Br(), html.Br(),
+            ]
+        ),
+        color="info", outline=True
+    )
+
+
+    result_card = dbc.Card(
+        dbc.CardBody(
+            [
+                html.H4("Results", className="card-title"),
                 dcc.Loading(
                     id="loading-1",
-                    type="graph",
+                    type="circle",
                     children=html.Div(id="loading-output-1")
                 ),
-                html.Br(),
-                html.Hr(),
-                html.Br(),
-                dbc.Col(html.Div(id='output-div'))
-            ],
-            align="center"
+                dbc.Col(html.Div(id='output-div', style={"width": "100%"}))
+            ]
         ),
-        justify="center"
+        color="info", outline=True
+    )
+
+    return dbc.Row(
+        dbc.Col(
+            children=[
+                location,
+                dbc.Row(
+                    dbc.Col(dbc.CardDeck(
+                        [
+                            text_card,
+                            search_card
+                        ]
+                    ), width=12),
+                    className="mb-4",
+                ),
+                dbc.Row(
+                    dbc.Col(result_card, width=12),
+                    className="mb-20",
+                )
+            ]
+        )
     )
 
 
@@ -155,13 +191,13 @@ def update_output(clicks, keyword, nr_videos, nr_users, graph_type):
         ranks = network.compute_page_rank()
         # network.compute_betweenness_centrality()
 
-        columns = ['Value', 'Name']
+        columns = ['Rank', 'Value', 'Name']
         values = []
         index = 0
         selected_nodes = []
         for u_id in sorted(ranks, key=ranks.get, reverse=True):
             selected_nodes.append(u_id)
-            val = {"Value": check_value(ranks, u_id), 'Name': check_value(labels, u_id)}
+            val = {"Rank": index+1, "Value": check_value(ranks, u_id), 'Name': check_value(labels, u_id)}
             values.append(val)
             index += 1
             if index >= limit:
@@ -182,7 +218,8 @@ def update_output(clicks, keyword, nr_videos, nr_users, graph_type):
                 html.H5("Users Graph"),
                 dcc.Graph(
                     id='network-graph',
-                    figure=fig),
+                    figure=fig,
+                    responsive=True),
                 html.Br(),
                 html.Hr(),
                 html.Br(),
@@ -208,7 +245,12 @@ def create_data_table_network(values, columns):
         data=values,
         sort_action="native",
         sort_mode='single',
-        page_size=20
+        page_size=20,
+        style_cell={
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+            'maxWidth': 0
+        }
     )
     return table
 

eebe9c9b9fde3e27ce756f7a27e1f8dd5413c6f2
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-07-14 19:15:23 +0300

    Removed sticky footer

--- application/message_logger.py
+++ application/message_logger.py
@@ -1,4 +1,6 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
+
 from google.cloud.logging import Client
 
 # google cloud logging client
@@ -17,8 +19,8 @@ class MessageLogger:
         """
         self.__logger = logging.getLogger(module_name)
         self.__logger.setLevel(logging.DEBUG)
-        self.__console_handler = client.get_default_handler()
-        # self.__console_handler = TimedRotatingFileHandler("logs/app_logs.log", when="midnight") # for local logging
+        #self.__console_handler = client.get_default_handler()
+        self.__console_handler = TimedRotatingFileHandler("logs/app_logs.log", when="midnight") # for local logging
         self.__console_handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.__console_handler.setFormatter(formatter)

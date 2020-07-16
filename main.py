04ed950dacd4d04d22416e64127f51cb634a45e5
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-07-16 14:55:21 +0300

    Added dashboard page, updated navbar for mobile use

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask_login import current_user
 from application.message_logger import MessageLogger
 from pages import (
     home,
+    dashboard,
     profile,
     analysis
 )
@@ -20,18 +21,31 @@ from pages.auth_pages import (
 )
 from server import app, server
 
-header = dbc.NavbarSimple(
-    children=[
-            dbc.NavItem(dbc.NavLink("Home", href="/home", style={"color": "white"})),
-            dbc.NavItem(dbc.NavLink("Analysis", href="/analysis", style={"color": "white"})),
-            html.Div(id='dropdown-container'),
-            dbc.NavItem(dbc.NavLink('Login', id='user-action', href='/login', style={"color": "white"}))
-    ],
-    brand="Influential Users",
-    brand_href="/home",
+
+menu_bar = [
+    dbc.NavLink("Home", href="/home", className="ml-auto"),
+    dbc.NavLink("Dash", href="/dashboard", style={"margin": "0"}),
+    dbc.NavLink("Analysis", href="/analysis", style={"margin": "0"}),
+    html.Div(id='dropdown-container', style={"margin": "0"}),
+    dbc.NavLink('Login', id='user-action', href='/login', style={"margin": "0"}),
+]
+
+header = dbc.Navbar(
+    dbc.Container([
+        html.A(
+            children=[
+                html.Img(src="static/images/logo.png", height="40px"),
+                dbc.NavbarBrand("socialinfluencers", className="ml-2"),
+            ],
+            href="/home",
+            className="navbar-brand"
+        ),
+        dbc.NavbarToggler(id="navbar-toggler"),
+        dbc.Collapse(menu_bar, id="navbar-collapse", navbar=True, className="mx-auto"),
+    ]),
     color="primary",
     dark=True,
-    className="mb-5",
+    className="ml-auto"
 )
 
 footer = html.Footer(
@@ -46,8 +60,7 @@ footer = html.Footer(
                         html.Li("·", className="list-inline-item"),
                         html.Li(html.A("Terms of use", href="#"), className="list-inline-item"),
                     ], className="list-inline mb-2"),
-                    html.P('© Influential users 2020. All Rights Reserved to Alexandru Grigoras.',
-                           className="text-muted small mb-4 mb-lg-0")
+                    html.P('socialinfluencers © 2020. All Rights Reserved.', className="text-muted small mb-4 mb-lg-0")
                 ], className="col-lg-6 h-100 text-center text-lg-left my-auto"
                 ),
                 html.Div([
@@ -64,19 +77,17 @@ footer = html.Footer(
             ])
         )
     ],
-    className="footer",
+    className="footer bg-light",
 )
 
 app.layout = html.Div(
     [
         header,
         html.Div(
-            [
-                dbc.Container(
-                    id='page-content'
-                ),
-                html.Br()
-            ]
+            html.Div(
+                id='page-content',
+            ),
+            className="wrapper bg-light"
         ),
         footer,
         dcc.Location(id='base-url', refresh=False),
@@ -106,9 +117,11 @@ def router(pathname):
     # app pages
     elif pathname == '/' or pathname == '/home':
         return home.layout()
-    elif pathname == '/profile' or pathname == '/profile':
+    elif pathname == '/dashboard':
+        return dashboard.layout()
+    elif pathname == '/profile':
         return profile.layout()
-    elif pathname == '/analysis' or pathname == '/analysis':
+    elif pathname == '/analysis':
         return analysis.layout()
 
     return html.Div(['404 - That page does not exist.', html.Br(), dcc.Link('Login', href='/login')])
@@ -125,17 +138,36 @@ def profile_link(content, children):
     if current_user.is_authenticated:
         new_dropdown = dbc.DropdownMenu(
             children=[
-                dbc.DropdownMenuItem("Profile", href='/profile'),
-                dbc.DropdownMenuItem("Logout", href="/logout"),
+                dbc.DropdownMenuItem([
+                    html.I(className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"),
+                    "Profile"
+                ], href='/profile'),
+                dbc.DropdownMenuItem(divider=True),
+                dbc.DropdownMenuItem([
+                    html.I(className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"),
+                    "Logout"
+                ], href="/logout"),
             ],
+            className="mx-auto",
             nav=True,
             in_navbar=True,
-            toggle_style={"color": "white"},
+            style={"list-style-type": "none"},
             label=current_user.first
         )
         return new_dropdown
     else:
         return ''
+
+
+@app.callback(
+    Output("navbar-collapse", "is_open"),
+    [Input("navbar-toggler", "n_clicks")],
+    [State("navbar-collapse", "is_open")],
+)
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
 
 
 @app.callback(

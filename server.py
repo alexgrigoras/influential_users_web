35e7f7fae3ada21fc543f7bd7cb7adecab271bda
alexgrigoras <alex_grigoras_10@yahoo.com>
2020-07-16 14:54:35 +0300

    Added ab-admin-2 css

--- server.py
+++ server.py
@@ -17,7 +17,8 @@ app = dash.Dash(
         "vendor/fontawesome-free/css/all.min.css",
         "vendor/simple-line-icons/css/simple-line-icons.css",
         "https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic",
-        "css/landing-page.min.css"
+        "css/landing-page.min.css",
+        "css/sb-admin-2.min.css"
     ],
     meta_tags=[
         {

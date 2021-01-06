2c4d7e8dc5d8854b927800f6c76a6777b385d7b5
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-01-06 21:40:21 +0200

    Removed stylesheets (are automatically loaded)

--- server.py
+++ server.py
@@ -14,11 +14,6 @@ app = dash.Dash(
     __name__,
     external_stylesheets=[
         dbc.themes.COSMO,
-        "vendor/fontawesome-free/css/all.min.css",
-        "vendor/simple-line-icons/css/simple-line-icons.css",
-        "https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic",
-        "css/landing-page.min.css",
-        "css/sb-admin-2.min.css"
     ],
     meta_tags=[
         {

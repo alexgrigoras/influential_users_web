ad9322b235bdc430fbaf66cf4326c8d4d5555fc7
Alexandru Grigoras <AG-MobileStation>
2021-06-16 22:07:51 +0300

    Cleaned keys

--- utilities/keys.py
+++ utilities/keys.py
@@ -1,4 +1,4 @@
-MAILJET_API_KEY = ''        # insert key
-MAILJET_API_SECRET = ''     # insert secret
+MAILJET_API_KEY = 'ea76fe4292c42a363afef7845555f551'
+MAILJET_API_SECRET = 'ac3df47795af2c6e5fee01ed2dc6c521'
 
-FROM_EMAIL = ''             # insert email
+FROM_EMAIL = 'influential-users@yandex.com'
